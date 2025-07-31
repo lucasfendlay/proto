37dcfc147f60d85fef97ee8eac8d352cc3729fdd
lucasfendlay <lucasfendlay@gmail.com>
2025-07-31 17:42:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const path = require('path');
 
 const app = express();
